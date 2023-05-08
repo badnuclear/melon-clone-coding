24993117bf1840c8d7b8c49ed72d472e2cd4643f
김민균 <alsrbs4024@gmail.com>
2023-05-08 12:05:00 +0900

    Part 3 인기 차트 구현및 유튜브 API 구현

--- src/controllers/songController.js
+++ src/controllers/songController.js
@@ -1,20 +1,50 @@
-import { async } from "regenerator-runtime";
+import { google } from "googleapis";
 import Song from "../models/Song";
-import { apiVideo } from "../youtubeApi";
 
-//뮤직 리스트 db에 넣기
-export const musicList = async (req, res) => {
-  await Song.deleteMany({});
-  const songs = await apiVideo();
-  Song.insertMany(songs);
-  console.log(songs);
-  return res.send("<h1> Update </h1>");
+//yutube API
+const youtube = google.youtube({
+  version: "v3",
+  auth: process.env.YOUTUBE_API,
+});
+
+export const musiChart = (req, res) => {
+  const apiKey = process.env.YOUTUBE_API;
+  const playlistId = process.env.PLAYLIST_ID;
+  youtube.playlistItems.list(
+    {
+      key: apiKey,
+      part: "snippet",
+      p: "HomeComeBear",
+      playlistId,
+      maxResults: 30,
+    },
+    (error, data) => {
+      if (error) {
+        console.log(error);
+        res.status(500).send("Error is Youtube Api");
+      } else {
+        const playlistItems = data.data.items;
+
+        playlistItems.forEach((item) => {
+          const playlistItem = new Song({
+            videoId: item.snippet.resourceId.videoId,
+            title: item.snippet.title,
+            description: item.snippet.description,
+            thumbnails: item.snippet.thumbnails.default.url,
+            views: 0,
+          });
+          playlistItem.save();
+        });
+      }
+    }
+  );
 };
+musiChart();
 
 export const home = async (req, res) => {
   try {
-    const list = Song.find({}).sort({ views: -1 });
-    return res.render("home", { pageTitle: "Home", list: list });
+    const songs = await Song.find({}).limit(10);
+    return res.render("home", { pageTitle: "Home", data: songs });
   } catch (error) {
     return res.render("Server Error");
   }
