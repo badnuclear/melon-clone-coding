2d6a0f60b4fadfc9137b5f14540c06581a1ce753
김민균 <alsrbs4024@gmail.com>
2023-05-09 23:45:43 +0900

    카카오 소셜 로그인/로그아웃 구현 및 Header CSS 디자인 정리

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import { async } from "regenerator-runtime";
+import fetch from "cross-fetch";
 import User from "../models/User";
 
 //소셜 카카오 로그인
@@ -6,7 +7,7 @@ export const startKakaoLogin = (req, res) => {
   const baseUrl = "https://kauth.kakao.com/oauth/authorize";
   const config = {
     client_id: process.env.KAKAO_CLIENT,
-    redirect_uri: "http://localhost:4000/users/kakao/finish",
+    redirect_uri: `http://localhost:4000/users/kakao/finish`,
     response_type: "code",
   };
   const params = new URLSearchParams(config).toString();
@@ -15,7 +16,7 @@ export const startKakaoLogin = (req, res) => {
   return res.redirect(finalUrl);
 };
 export const finishKakaoLogin = async (req, res) => {
-  const baseUrl = "https://kauth.kakao.com/oauth/authorize";
+  const baseUrl = "https://kauth.kakao.com/oauth/token";
   const config = {
     grant_type: "authorization_code",
     client_id: process.env.KAKAO_CLIENT,
@@ -31,10 +32,74 @@ export const finishKakaoLogin = async (req, res) => {
       method: "POST",
     })
   ).json();
+
   if ("access_token" in tokenResponse) {
     const apiUrl = "https://kapi.kakao.com";
     const { access_token } = tokenResponse;
+
+    const userToken = await (
+      await fetch(`${apiUrl}/v1/user/access_token_info`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      })
+    ).json();
+
+    if (userToken.msg === "no authentication key!") {
+      console.log(userToken.msg);
+      return res.redirect("/");
+    }
+
+    const userData = await (
+      await fetch(`${apiUrl}/v2/user/me`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+          target_id_type: "user_id",
+          target_id: userToken.id,
+          Accept: "application/json",
+        },
+      })
+    ).json();
+
+    //카카오 계정
+    const kakaoAccount = userData.kakao_account;
+    const kakaoProfile = kakaoAccount.profile;
+
+    //카카오 이메일
+    if (
+      kakaoAccount.is_email_valid === false ||
+      kakaoAccount.is_email_verified === false
+    ) {
+      console.log(
+        "이메일이 인증되지 않았거나 다른 계정에 사용된 이메일입니다. 다시 확인해주세요"
+      );
+      return res.redirect("/");
+    }
+
+    let user = await User.findOne({ email: kakaoAccount.email });
+    if (!user) {
+      user = await User.create({
+        name: kakaoProfile.nickname,
+        username: kakaoProfile.nickname,
+        email: kakaoAccount.email,
+      });
+    }
+    req.flash("success", "로그인 성공!");
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect("/");
+  } else {
+    return res.redirect("/");
   }
+};
+
+//로그아웃
+export const logout = (req, res) => {
+  req.flash("info", "Log out");
+  req.session.destroy();
+  return res.redirect("/");
 };
 
 export const see = async (req, res) => {
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,10 +3,12 @@ import {
   finishKakaoLogin,
   see,
   startKakaoLogin,
+  logout,
 } from "../controllers/userController";
 
 const userRouter = express.Router();
 
+userRouter.get("/logout", logout);
 userRouter.get("/:id", see);
 userRouter.get("/kakao/start", startKakaoLogin);
 userRouter.get("/kakao/finish", finishKakaoLogin);
